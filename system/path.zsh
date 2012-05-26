891994973b7e642c335bfd03bb922d9f703b4741
Sandro Padin <sandropadin@gmail.com>
2012-05-26 12:39:12 -0500

    * removing postgresql path.

--- system/path.zsh
+++ system/path.zsh
@@ -2,7 +2,7 @@
 export PATH=/usr/local/bin:/usr/local/sbin:/usr/local/mysql/bin:/usr/bin:/bin:/usr/sbin:/sbin:/usr/local/git/bin:/usr/X11/bin:/Users/sandropadin/bin
 export PATH=$HOME/.rbenv/bin:$PATH
 export PATH=/usr/local/pgsql/bin:$PATH
-export PATH=/Applications/Postgres.app/Contents/MacOS/bin:$PATH
+# export PATH=/Applications/Postgres.app/Contents/MacOS/bin:$PATH
 export PATH="./bin:$HOME/.rbenv/shims:$DOTFILES/bin:$PATH"
 
 export MANPATH="/usr/local/man:/usr/local/mysql/man:/usr/local/git/man:$MANPATH"
