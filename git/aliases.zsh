284ad75cb89be091c0a900b3c1ca72dd2af3adf2
Sandro Padin <sandropadin@gmail.com>
2013-01-04 20:14:47 -0600

    Adding ggpull and ggpush

--- git/aliases.zsh
+++ git/aliases.zsh
@@ -18,3 +18,14 @@ alias gb='git branch'
 alias gs='git status' # upgrade your git if -sb breaks for you. it's fun.
 alias gst='git status -sb'
 alias grm="git status | grep deleted | awk '{print \$3}' | xargs git rm"
+
+function current_branch() {
+  ref=$(git symbolic-ref HEAD 2> /dev/null) || \
+  ref=$(git rev-parse --short HEAD 2> /dev/null) || return
+  echo ${ref#refs/heads/}
+}
+
+alias ggpull='git pull origin $(current_branch)'
+compdef ggpull=git
+alias ggpush='git push origin $(current_branch)'
+compdef ggpush=git
