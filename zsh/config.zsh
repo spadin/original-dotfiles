8bae75a022701c530cddd0e37eeea78fdde373a0
Sandro Padin <sandropadin@gmail.com>
2013-03-08 09:30:38 -0600

    Adds new bindings for end and beginning of line

--- zsh/config.zsh
+++ zsh/config.zsh
@@ -44,3 +44,7 @@ bindkey '^[[5C' end-of-line
 bindkey '^[[3~' delete-char
 bindkey '^[^N' newtab
 bindkey '^?' backward-delete-char
+
+
+bindkey '^[[1;5D' beginning-of-line # ctrl-<left arrow>
+bindkey '^[[1;5C' end-of-line       # ctrl-<right arrow>
